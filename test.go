4f7db88f1ac3d098969089f97220abb3172ead3d
Jxck <block.rxckin.beats@gmail.com>
2013-09-15 18:23:56 +0900

    update

--- test.go
+++ test.go
@@ -10,23 +10,23 @@ func init() {
 }
 
 func main() {
-	conn, _ := net.Dial("tcp", "jxck.io:8080")
+	conn, err := net.Dial("tcp", "jxck.io:8080")
+	log.Println(err)
 
 	n, e := conn.Write([]byte("GET / HTTP/1.1\r\n"))
 	log.Println(n, e)
-	n, e = conn.Write([]byte("Host: jxck.io\r\n"))
-	log.Println(n, e)
 	n, e = conn.Write([]byte("Connection: Upgrade, HTTP2-Settings\r\n"))
 	log.Println(n, e)
-	n, e = conn.Write([]byte("upgrade: http/2.0\r\n"))
+	n, e = conn.Write([]byte("Upgrade: HTTP-draft-06/2.0\r\n"))
 	log.Println(n, e)
-	n, e = conn.Write([]byte("HTTP2-Settings: AAgEAAAAAAAAAAAEAAAAxA==\r\n"))
+	// n, e = conn.Write([]byte("HTTP2-Settings: AAgEAAAAAAAAAAAEAAAAxA==\r\n"))
+	n, e = conn.Write([]byte("HTTP2-Settings: AAAABAAAAGQAAAAHAAD//w==\r\n"))
 	log.Println(n, e)
 	n, e = conn.Write([]byte("\r\n"))
 	log.Println(n, e)
 
-	buf := make([]byte, 16, 16)
-	n, err := conn.Read(buf)
+	buf := make([]byte, 1000)
+	n, err = conn.Read(buf)
 
-	log.Printf("%v %v %v", n, err, buf)
+	log.Printf("%v %v %v", n, err, string(buf))
 }
