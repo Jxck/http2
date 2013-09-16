6d31abade806be4c18b547dbfddd08190367b8d7
Jxck <block.rxckin.beats@gmail.com>
2013-09-17 01:59:03 +0900

    update main

--- main/main.go
+++ main/main.go
@@ -64,33 +64,40 @@ func SendHeaders(conn net.Conn) {
 }
 
 func main() {
-	conn, _ := net.Dial("tcp", "jxck.io:8080") // err
+	conn, err := net.Dial("tcp", "localhost:8080") // err
+	log.Println(err)
 
 	conn.Write([]byte("GET / HTTP/1.1\r\n"))                      // err
 	conn.Write([]byte("Connection: Upgrade, HTTP2-Settings\r\n")) // err
 	conn.Write([]byte("Upgrade: HTTP-draft-06/2.0\r\n"))          // err
 	//conn.Write([]byte("HTTP2-Settings: " + setting() + "\r\n"))   // err
 	conn.Write([]byte("HTTP2-Settings: AAAABAAAAGQAAAAHAAD\r\n")) // err
-	conn.Write([]byte("\r\n"))                                    // err
+	conn.Write([]byte("\r\n")) // err
+
 
 	fh := &FrameHeader{}
 	fh.Decode(conn)
 
+	conn.Write([]byte("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")) // err
+
+	b := make([]byte, 100)
+	n, err := conn.Read(b)
+	log.Println(n, err, b)
+
 	// SendSettings
-	fh = &FrameHeader{}
-	fh.Length = 8
-	fh.Type = 0x4
-	fh.Flags = 0
-	fh.StreamId = 0
+	//fh = &FrameHeader{}
+	//fh.Length = 8
+	//fh.Type = 0x4
+	//fh.Flags = 0
+	//fh.StreamId = 0
 
-	setting := Setting{0, 4, 1024}
-	sf := SettingsFrame{*fh, []Setting{setting}}
-	log.Print(&sf)
-	log.Println(sf.Encode().Bytes())
+	//setting := Setting{0, 4, 1024}
+	//sf := SettingsFrame{*fh, []Setting{setting}}
+	//fmt.Print(&sf)
+	//log.Println(sf.Encode().Bytes())
 
-	n, e := conn.Write(sf.Encode().Bytes())
-	log.Println(n, e)
+	//n, e := conn.Write(sf.Encode().Bytes())
+	//log.Println(n, e)
 
-	SendHeaders(conn)
-
+	//SendHeaders(conn)
 }
