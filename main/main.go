ab658d82c52fd3d17e92c487c71e31b9198f4503
Jxck <block.rxckin.beats@gmail.com>
2013-09-22 17:02:23 +0900

    up

--- main/main.go
+++ main/main.go
@@ -1,101 +1,73 @@
 package main
 
 import (
-	"encoding/base64"
-	. "github.com/jxck/hpack"
-	. "github.com/jxck/http2"
+	"github.com/jxck/hpack"
 	"log"
 	"net"
-	"net/http"
 )
 
 func init() {
 	log.SetFlags(log.Lshortfile)
 }
 
-func setting() string {
-	// [0 8 4 0, 0 0 0 0, 0 0 0 4, 127 255 255 255]
-	buf := []byte{
-		0, 8, 4, 0,
-		// 00000000 00001000,00000100,00000000
-		0, 0, 0, 0,
-		// 00000000 00000000 00000000 00000000
-		0, 0, 0, 4,
-		// 00000000 00000000 00000000 00000100
-		0, 0, 0, 0xc4,
-		// 00000000 00000000 00000000 11000100
-	}
-	str := base64.StdEncoding.EncodeToString(buf)
-
-	// log.Printf("%v", str) // AAgEAAAAAAAAAAAEAAAAxA==
-
-	return str
-}
-
-func SendHeaders(conn net.Conn) {
-	headers := http.Header{
-		"Scheme":     []string{"https"},
-		"Host":       []string{"jxck.io:8080"},
-		"Path":       []string{"/"},
-		"Method":     []string{"GET"},
-		"User-Agent": []string{"http2cat"},
-		"Cookie":     []string{"xxxxxxx2"},
-		"Accept":     []string{"*/*"},
-	}
-
-	client := NewContext()
-	wire := client.Encode(headers)
-
-	fh := &FrameHeader{}
-	fh.Length = uint16(len(wire))
-	fh.Type = 0x1
-	fh.Flags = 0x1 // END_STREAM
-	fh.StreamId = 1
-
-	hf := &HeadersFrame{*fh, 0, wire}
-	log.Println(hf.Encode().Bytes())
-
-	n, e := conn.Write(hf.Encode().Bytes())
-	log.Println(n, e)
-
-	b := make([]byte, 100)
-	n, err := conn.Read(b)
-	log.Println(n, err, b)
-}
-
 func main() {
-	conn, err := net.Dial("tcp", "106.186.112.116:80") // err
-	log.Println(err)
+	conn, _ := net.Dial("tcp", "106.186.112.116:80") // err
 
-	conn.Write([]byte("GET / HTTP/1.1\r\n"))                      // err
-	conn.Write([]byte("Connection: Upgrade, HTTP2-Settings\r\n")) // err
-	conn.Write([]byte("Upgrade: HTTP-draft-06/2.0\r\n"))          // err
-	conn.Write([]byte("HTTP2-Settings: AAAABAAAAGQAAAAHAAD\r\n")) // err
-	conn.Write([]byte("\r\n"))                                    // err
+	conn.Write([]byte("GET / HTTP/1.1\r\n"))                         // err
+	conn.Write([]byte("Host: 106.186.112.116:80\r\n"))               // err
+	conn.Write([]byte("Connection: Upgrade, HTTP2-Settings\r\n"))    // err
+	conn.Write([]byte("Upgrade: HTTP-draft-06/2.0\r\n"))             // err
+	conn.Write([]byte("HTTP2-Settings: AAAABAAAAGQAAAAHAAD__w\r\n")) // err
+	conn.Write([]byte("Accept: */*\r\n"))                            // err
+	conn.Write([]byte("\r\n"))                                       // err
 
-	fh := &FrameHeader{}
-	fh.Decode(conn)
+	b := make([]byte, 76)
+	conn.Read(b)
+	// Upgrade Success :)
 
 	conn.Write([]byte("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")) // err
 
-	b := make([]byte, 100)
+	b = make([]byte, 36)
+	conn.Read(b)
+	//log.Println(b)
+	/*
+	   ====
+	   0 16 4 0 (length=16, type=4, flag=0) Settings Frame
+	   0 0 0 0 (R=0, StreamId=0)
+
+	   0 0 0 4 (Reserved=0, SettingId=4)
+	   0 0 0 100 SETTING_MAX_CONCURRENT_STREAM=100
+
+	   0 0 0 7
+	   0 0 255 255 SETTING_INITIAL_WINDOW_SIZE=65535
+	   ====
+	   0 4 9 0 (length=4, type=9, flag=0) Window Update Frame
+	   0 0 0 0 (StreamId=0)
+	   59 154 202 7
+	   00111011, 10011010, 11001010, 00000111=1000000007
+	   ====
+	*/
+
+	// [MAX_CONCURRENT_STREAMS(4):100]
+	// [INITIAL_WINDOW_SIZE(7):65535]
+
+	//0 16 4 0
+	//0  0 0 0
+	//0  0 0 4
+	//     100
+	//0  0 0 7
+	//   65535
+	conn.Write([]byte{0x0, 0x10, 0x4, 0x0,
+		0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x4,
+		0x0, 0x0, 0x0, 0x64,
+		0x0, 0x0, 0x0, 0x7,
+		0x0, 0x0, 0xFF,
+	}) // err
+
+	b = make([]byte, 2000)
 	n, err := conn.Read(b)
-	log.Println(n, err, b)
-
-	// SendSettings
-	//fh = &FrameHeader{}
-	//fh.Length = 8
-	//fh.Type = 0x4
-	//fh.Flags = 0
-	//fh.StreamId = 0
-
-	//setting := Setting{0, 4, 1024}
-	//sf := SettingsFrame{*fh, []Setting{setting}}
-	//fmt.Print(&sf)
-	//log.Println(sf.Encode().Bytes())
-
-	//n, e := conn.Write(sf.Encode().Bytes())
-	//log.Println(n, e)
-
-	//SendHeaders(conn)
+	log.Println(n, err)
+	server := hpack.NewResponseContext()
+	server.Decode(b[8:n])
 }
