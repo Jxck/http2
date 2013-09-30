74fb2be58ed368442da4ba93915c040ef6f62674
Jxck <block.rxckin.beats@gmail.com>
2013-09-30 23:44:10 +0900

    use bufio.Writer

--- main/main.go
+++ main/main.go
@@ -25,21 +25,25 @@ func main() {
 
 	conn, _ := net.Dial("tcp", "106.186.112.116:80") // err
 
-	conn.Write([]byte("GET / HTTP/1.1\r\n"))                                           // err
-	conn.Write([]byte("Host: 106.186.112.116:80\r\n"))                                 // err
-	conn.Write([]byte("Connection: Upgrade, HTTP2-Settings\r\n"))                      // err
-	conn.Write([]byte("Upgrade: HTTP-draft-06/2.0\r\n"))                               // err
-	conn.Write([]byte("HTTP2-Settings: " + settingsFrame.PayloadBase64URL() + "\r\n")) // err
-	conn.Write([]byte("Accept: */*\r\n"))                                              // err
-	conn.Write([]byte("\r\n"))                                                         // err
-
+	bw := bufio.NewWriter(conn)
 	br := bufio.NewReader(conn)
+
+	bw.WriteString("GET / HTTP/1.1\r\n")                                           // err
+	bw.WriteString("Host: 106.186.112.116:80\r\n")                                 // err
+	bw.WriteString("Connection: Upgrade, HTTP2-Settings\r\n")                      // err
+	bw.WriteString("Upgrade: HTTP-draft-06/2.0\r\n")                               // err
+	bw.WriteString("HTTP2-Settings: " + settingsFrame.PayloadBase64URL() + "\r\n") // err
+	bw.WriteString("Accept: */*\r\n")                                              // err
+	bw.WriteString("\r\n\r\n")                                                     // err
+	bw.Flush()                                                                     // err
+
 	res, _ := http.ReadResponse(br, &http.Request{Method: "GET"}) // err
 
 	fmt.Println(Green(http2.ResponseString(res)))
 	Debug(Red("Upgrade Success :)"))
 
-	conn.Write([]byte("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n")) // err
+	bw.WriteString("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n") // err
+	bw.Flush()                                         // err
 
 	framer := &http2.Framer{
 		RW: conn,
