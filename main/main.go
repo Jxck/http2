a2ccb87c445b86dd6a93c658531cb145b329906e
Jxck <block.rxckin.beats@gmail.com>
2013-10-10 00:08:38 +0900

    add -d flag for post message

--- main/main.go
+++ main/main.go
@@ -11,12 +11,14 @@ import (
 )
 
 var nullout, upgrade bool
+var post string
 
 func init() {
 	log.SetFlags(log.Lshortfile)
 	f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	f.BoolVar(&nullout, "n", false, "null output")
 	f.BoolVar(&upgrade, "u", false, "upgrade")
+	f.StringVar(&post, "d", "", "send post data")
 	f.Parse(os.Args[1:])
 	for 0 < f.NArg() {
 		f.Parse(f.Args()[1:])
@@ -33,16 +35,23 @@ func main() {
 		Transport: transport,
 	}
 
-	// GET
-	// res, err := client.Get(url)
-
-	// POST
-	buf := bytes.NewBufferString("hello world")
-	res, err := client.Post(url, "text/plain", buf)
-
-	if err != nil {
-		log.Println(err)
+	var res *http.Response
+	var err error
+	if post == "" {
+		// GET
+		res, err = client.Get(url)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		// POST
+		buf := bytes.NewBufferString(post)
+		res, err = client.Post(url, "text/plain", buf)
+		if err != nil {
+			log.Println(err)
+		}
 	}
+
 	defer res.Body.Close()
 	if !nullout {
 		body, err := ioutil.ReadAll(res.Body)
