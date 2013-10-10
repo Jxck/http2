94a206666ea8a9fbb19a859ed0812d39a924b6be
Jxck <block.rxckin.beats@gmail.com>
2013-10-10 21:10:30 +0900

    update main/server.go

--- main/server.go
+++ main/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	_ "github.com/jxck/http2"
 	"log"
 	"net/http"
 )
@@ -14,18 +15,24 @@ func init() {
 type Hello struct{}
 
 func (h *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello!")
+	fmt.Fprint(w, "Hello HTTP2.0!")
 }
 
-var httpHandler http.Handler = &Hello{}
+var handler http.Handler = &Hello{}
 
 // var httpHandler http.Handler = http.FileServer(http.Dir("."))
 
 func main() {
 	flag.Parse()
-	port := ":" + flag.Args()[0]
+	args := flag.Args()
+	if len(args) == 0 {
+		log.Fatal("no port number specified")
+	}
 
-	err := http.ListenAndServe(port, httpHandler)
+	addr := ":" + args[0]
+
+	log.Printf("server starts on port %s\n", addr)
+	err := http.ListenAndServe(addr, handler)
 	if err != nil {
 		log.Fatal(err)
 	}
