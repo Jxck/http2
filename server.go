b81be27d117b5b05ac207d0aaf0a8412dae7729e
Jxck <block.rxckin.beats@gmail.com>
2013-10-11 00:33:17 +0900

    update HeandeConnection

--- server.go
+++ server.go
@@ -35,10 +35,27 @@ func HandleConnection(conn net.Conn) {
 	defer conn.Close()
 	Conn := NewConn(conn)
 	req := Conn.ReadRequest()
-	log.Println(req)
-	//	for {
-	//		frame := Conn.ReadFrame()
-	//		log.Println(frame)
-	//	}
+
+	log.Println(req.Header.Get("Connection"))
+	log.Println(req.Header.Get("Upgrade"))
+	log.Println(req.Header.Get("Http2-Settings"))
+
+	upgrade := `HTTP/1.1 101 Switching Protocols
+Connection: Upgrade
+Upgrade: HTTP-draft-06/2.0
+
+`
+	log.Printf("%q", upgrade)
+	Conn.WriteString(upgrade)
+
+	settings := map[SettingsId]uint32{
+		SETTINGS_MAX_CONCURRENT_STREAMS: 100,
+		SETTINGS_INITIAL_WINDOW_SIZE:    DEFAULT_WINDOW_SIZE,
+	}
+	Conn.SendSettings(settings)
+	for c := 0; c < 4; c++ {
+		Conn.ReadFrame()
+	}
+
 	return
 }
