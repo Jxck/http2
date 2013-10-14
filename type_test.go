b41ee9225a9fe587fe9364a3077f9c1402d1d215
Jxck <block.rxckin.beats@gmail.com>
2013-10-14 11:26:03 +0900

    add test DataFrame

--- type_test.go
+++ type_test.go
@@ -23,3 +23,24 @@ func TestFrameHeader(t *testing.T) {
 		t.Errorf("got %v\nwant %v", actual, expected)
 	}
 }
+
+func TestDataFrame(t *testing.T) {
+	b := []byte("hello")
+	expected := NewDataFrame(1, 2)
+	expected.Length = uint16(len(b))
+	expected.Data = b
+
+	buf := bytes.NewBuffer([]byte{})
+	expected.Write(buf)
+
+	fh := &FrameHeader{}
+	fh.Read(buf)
+
+	actual := &DataFrame{}
+	actual.FrameHeader = fh
+	actual.Read(buf)
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
