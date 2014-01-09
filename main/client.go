4f0d25521239a3d6360477a398f91d50f48cac85
Jxck <block.rxckin.beats@gmail.com>
2014-01-09 12:58:51 +0900

    fix npn client

--- main/client.go
+++ main/client.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"bytes"
+	"crypto/tls"
 	"flag"
-	"fmt"
-	"github.com/jxck/http2"
 	"github.com/jxck/logger"
-	"io/ioutil"
+	"io"
 	"log"
-	"net/http"
 	"os"
 )
 
@@ -31,40 +28,90 @@ func init() {
 	logger.LogLevel(loglevel)
 }
 
+// func main() {
+// 	url := os.Args[1]
+//
+// 	transport := &http2.Transport{
+// 		Upgrade: upgrade,
+// 		FlowCtl: flowctl,
+// 	}
+// 	client := &http.Client{
+// 		Transport: transport,
+// 	}
+//
+// 	var res *http.Response
+// 	var err error
+// 	if post == "" {
+// 		// GET
+// 		res, err = client.Get(url)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 	} else {
+// 		// POST
+// 		buf := bytes.NewBufferString(post)
+// 		res, err = client.Post(url, "text/plain", buf)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 	}
+//
+// 	defer res.Body.Close()
+// 	if !nullout {
+// 		body, err := ioutil.ReadAll(res.Body)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 		fmt.Println(string(body))
+// 	}
+// }
+
+// NPN Dial
+func DialNPN(address, certpath, keypath string) *tls.Conn {
+	// 証明書の設定
+	cert, err := tls.LoadX509KeyPair(certpath, keypath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TLS の設定(証明書検証無)
+	config := tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"http2.0/draft-09"},
+	}
+	conn, err := tls.Dial("tcp", address, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 接続確認
+	state := conn.ConnectionState()
+	log.Println("handshake: ", state.HandshakeComplete)
+	log.Println("protocol: ", state.NegotiatedProtocol)
+
+	return conn
+}
+
 func main() {
-	url := os.Args[1]
+	// セットアップ
+	address := "jxck.io:8443"
+	certpath, keypath := "keys/cert.pem", "keys/key.pem"
 
-	transport := &http2.Transport{
-		Upgrade: upgrade,
-		FlowCtl: flowctl,
-	}
-	client := &http.Client{
-		Transport: transport,
-	}
+	// Dial NPN
+	conn := DialNPN(address, certpath, keypath)
+	defer conn.Close() // err
 
-	var res *http.Response
-	var err error
-	if post == "" {
-		// GET
-		res, err = client.Get(url)
-		if err != nil {
-			log.Println(err)
-		}
-	} else {
-		// POST
-		buf := bytes.NewBufferString(post)
-		res, err = client.Post(url, "text/plain", buf)
-		if err != nil {
-			log.Println(err)
-		}
-	}
+	// データの送受信
+	message := "Hello\n"
 
-	defer res.Body.Close()
-	if !nullout {
-		body, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			log.Println(err)
-		}
-		fmt.Println(string(body))
+	n, err := io.WriteString(conn, message)
+	if err != nil {
+		log.Fatalf("write: %s", err)
 	}
+	log.Printf("wrote %q (%d bytes)", message, n)
+	reply := make([]byte, 256)
+	n, err = conn.Read(reply)
+	log.Printf("read %q (%d bytes)", string(reply[:n]), n)
+	log.Print("exiting")
 }
--- url.go
+++ url.go
@@ -3,7 +3,7 @@ package http2
 import (
 	"fmt"
 	"log"
-	urllib "net/url"
+	neturl "net/url"
 	"strings"
 )
 
@@ -11,31 +11,14 @@ func init() {
 	log.SetFlags(log.Lshortfile)
 }
 
+// Exted net/url with adding Port
 type URL struct {
-	*urllib.URL
+	*neturl.URL
 	Port string
 }
 
-func (url *URL) SplitHostPort() (err error) {
-	splitted := strings.Split(url.Host, ":")
-	switch {
-	case len(splitted) > 2:
-		return fmt.Errorf("parse error %q in HOST:PORT", url.Host)
-	case len(splitted) > 1:
-		url.Host, url.Port = splitted[0], splitted[1]
-		return
-	case url.Scheme == "https":
-		url.Host, url.Port = splitted[0], "443"
-		return
-	case url.Scheme == "http":
-		url.Host, url.Port = splitted[0], "80"
-		return
-	}
-	return
-}
-
 func NewURL(rawurl string) (*URL, error) {
-	u, err := urllib.Parse(rawurl)
+	u, err := neturl.Parse(rawurl)
 	if err != nil {
 		return nil, err
 	}
@@ -48,4 +31,19 @@ func NewURL(rawurl string) (*URL, error) {
 		return nil, err
 	}
 	return url, nil
+}
+
+func (url *URL) SplitHostPort() (err error) {
+	splitted := strings.Split(url.Host, ":")
+	switch {
+	case len(splitted) > 2:
+		err = fmt.Errorf("parse error %q in HOST:PORT", url.Host)
+	case len(splitted) > 1:
+		url.Host, url.Port = splitted[0], splitted[1]
+	case url.Scheme == "https":
+		url.Host, url.Port = splitted[0], "443"
+	case url.Scheme == "http":
+		url.Host, url.Port = splitted[0], "80"
+	}
+	return
 }
