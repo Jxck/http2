fc416413052121012aef66750ff6bed829052448
Jxck <block.rxckin.beats@gmail.com>
2014-01-12 19:18:52 +0900

    update ErrorCode

--- type.go
+++ type.go
@@ -372,11 +372,14 @@ const (
 	PROTOCOL_ERROR               = 1
 	INTERNAL_ERROR               = 2
 	FLOW_CONTROL_ERROR           = 3
+	SETTINGS_TIMEOUT             = 4
 	STREAM_CLOSED                = 5
-	FRAME_TOO_LARGE              = 6
+	FRAME_SIZE_ERROR             = 6
 	REFUSED_STREAM               = 7
 	CANCEL                       = 8
 	COMPRESSION_ERROR            = 9
+	CONNECT_ERROR                = 10
+	ENHANCE_YOUR_CALM            = 420
 )
 
 func (e ErrorCode) ToName() string {
