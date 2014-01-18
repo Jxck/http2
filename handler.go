32464736bf189658edaeaf6de67c828d547619ca
Jxck <block.rxckin.beats@gmail.com>
2014-01-18 20:44:54 +0900

    add http2.Handler type

--- handler.go
+++ handler.go
@@ -20,85 +20,104 @@ var defaultSettings = map[SettingsId]uint32{
 	SETTINGS_INITIAL_WINDOW_SIZE:    DEFAULT_WINDOW_SIZE,
 }
 
-func HandleConnection(conn net.Conn, handler http.Handler) {
+type Handler struct {
+	Conn    *Conn
+	Handler http.Handler
+}
+
+func (handler *Handler) HandShakeSSL() {
+	// Read Magic
+	handler.Conn.ReadMagic()
+
+	// Send SETTINGS
+	handler.Conn.SendSettings(defaultSettings)
+
+	handler.Conn.ReadFrame(hpack.REQUEST)
+	handler.Conn.ReadFrame(hpack.REQUEST)
+}
+
+func (handler *Handler) HandShake() (req *http.Request) {
+	req = handler.Conn.ReadRequest()
+
+	// TODO: parse/check settings
+	Debug("%s", req.Header.Get("Http2-Settings"))
+
+	upgrade := fmt.Sprintf(""+
+		"HTTP/1.1 101 Switching Protocols\r\n"+
+		"Connection: Upgrade\r\n"+
+		"Upgrade: %v\r\n"+
+		"\r\n",
+		Version)
+
+	handler.Conn.WriteString(upgrade)
+
+	// Send SETTINGS
+	handler.Conn.SendSettings(defaultSettings)
+
+	// Read Magic
+	handler.Conn.ReadMagic()
+
+	return req
+}
+
+func (handler *Handler) ServeHTTP(req *http.Request) {
+	// Send HEADERS
+	stream := handler.Conn.NewStream()
+	header := http.Header{}
+	header.Add("status", "200")
+	header.Add("content-type", "text/plain")
+
+	res := &Response{
+		Headers: header,
+		Body:    bytes.NewBuffer(make([]byte, 0)),
+	}
+
+	handler.Handler.ServeHTTP(res, req)
+
+	frame := NewHeadersFrame(END_HEADERS, 1)
+	frame.Headers = header
+
+	headerSet := hpack.ToHeaderSet(header)
+	frame.HeaderBlock = stream.Conn.ResponseContext.Encode(headerSet)
+	frame.Length = uint16(len(frame.HeaderBlock))
+	stream.Send(frame) // err
+
+	// Send DATA
+	data := NewDataFrame(0, 1)
+	data.Data = res.Body.Bytes()
+	data.Length = uint16(len(data.Data))
+	stream.Send(data)
+
+	data = NewDataFrame(END_STREAM, stream.Id)
+	stream.Send(data)
+}
+
+func HandleConnection(conn net.Conn, h http.Handler) {
 	Info("Handle Connection")
 	defer conn.Close() // err
 
-	// convert to http2.Conn
-	Conn := NewConn(conn)
+	handler := &Handler{
+		// convert to http2.Conn
+		Conn:    NewConn(conn),
+		Handler: h,
+	}
 
 	var req *http.Request
 	if SSL {
-		// Read Magic
-		Conn.ReadMagic()
-
-		// Send SETTINGS
-		Conn.SendSettings(defaultSettings)
-
-		Conn.ReadFrame(hpack.REQUEST)
-		Conn.ReadFrame(hpack.REQUEST)
-
+		handler.HandShakeSSL()
 	} else {
-		req = Conn.ReadRequest()
-
-		// TODO: parse/check settings
-		Debug("%s", req.Header.Get("Http2-Settings"))
-
-		upgrade := fmt.Sprintf(""+
-			"HTTP/1.1 101 Switching Protocols\r\n"+
-			"Connection: Upgrade\r\n"+
-			"Upgrade: %v\r\n"+
-			"\r\n",
-			Version)
-
-		Conn.WriteString(upgrade)
-
-		// Send SETTINGS
-		Conn.SendSettings(defaultSettings)
-
-		// Read Magic
-		Conn.ReadMagic()
+		req = handler.HandShake()
 	}
 
 	fin := make(chan bool)
 
 	// Send Routine
-	go func() {
-		// Send HEADERS
-		stream := Conn.NewStream()
-		header := http.Header{}
-		header.Add("status", "200")
-		header.Add("content-type", "text/plain")
-
-		res := &Response{
-			Headers: header,
-			Body:    bytes.NewBuffer(make([]byte, 0)),
-		}
-
-		handler.ServeHTTP(res, req)
-
-		frame := NewHeadersFrame(END_HEADERS, 1)
-		frame.Headers = header
-
-		headerSet := hpack.ToHeaderSet(header)
-		frame.HeaderBlock = stream.Conn.ResponseContext.Encode(headerSet)
-		frame.Length = uint16(len(frame.HeaderBlock))
-		stream.Send(frame) // err
-
-		// Send DATA
-		data := NewDataFrame(0, 1)
-		data.Data = res.Body.Bytes()
-		data.Length = uint16(len(data.Data))
-		stream.Send(data)
-
-		data = NewDataFrame(END_STREAM, stream.Id)
-		stream.Send(data)
-	}()
+	go handler.ServeHTTP(req)
 
 	// Recv Routine
 	go func() {
 		for c := 0; c < 4; c++ {
-			frame := Conn.ReadFrame(hpack.REQUEST)
+			frame := handler.Conn.ReadFrame(hpack.REQUEST)
 			_, ok := frame.(*GoAwayFrame)
 			if ok {
 				break
