649f89e437774038309110d499452a219100802d
Jxck <block.rxckin.beats@gmail.com>
2014-02-15 10:58:50 +0900

    refuctoring

--- conn.go
+++ conn.go
@@ -90,22 +90,6 @@ func (c *Conn) WriteLoop() { // err
 	}
 }
 
-func (c *Conn) SendSettings(settings map[SettingsId]uint32) { // err
-	c.WriteFrame(NewSettingsFrame(0, settings, 0)) // err
-}
-
-func (c *Conn) SendSettingsAck() { // err
-	c.WriteFrame(NewSettingsFrame(1, nil, 0)) // err
-}
-
-func (c *Conn) SendGoAway(errorCode ErrorCode) { // err
-	c.WriteFrame(NewGoAwayFrame(c.LastStreamId, errorCode, 0)) // err
-}
-
-func (c *Conn) SendWindowUpdate(incrementSize uint32) { // err
-	c.WriteFrame(NewWindowUpdateFrame(incrementSize, 0)) // err
-}
-
 func (c *Conn) WriteString(str string) { // err
 	c.Bw.WriteString(str) // err
 	c.Bw.Flush()          // err
--- stream.go
+++ stream.go
@@ -187,3 +187,19 @@ func (stream *Stream) WindowUpdate(size uint16) {
 		stream.Conn.WindowSize += threshold
 	}
 }
+
+func (c *Conn) SendSettings(settings map[SettingsId]uint32) { // err
+	c.WriteFrame(NewSettingsFrame(0, settings, 0)) // err
+}
+
+func (c *Conn) SendSettingsAck() { // err
+	c.WriteFrame(NewSettingsFrame(1, nil, 0)) // err
+}
+
+func (c *Conn) SendGoAway(errorCode ErrorCode) { // err
+	c.WriteFrame(NewGoAwayFrame(c.LastStreamId, errorCode, 0)) // err
+}
+
+func (c *Conn) SendWindowUpdate(incrementSize uint32) { // err
+	c.WriteFrame(NewWindowUpdateFrame(incrementSize, 0)) // err
+}
