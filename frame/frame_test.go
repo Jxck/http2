6c803972a2f8128b2f36071b8154f93725d04ebe
Jxck <block.rxckin.beats@gmail.com>
2014-09-21 13:20:42 +0900

    add rst stream test

--- frame/frame_test.go
+++ frame/frame_test.go
@@ -297,6 +297,58 @@ func TestRstStreamFrame(t *testing.T) {
 	assert.Equal(t, actual, expected)
 }
 
+func TestRstStreamCase(t *testing.T) {
+	var c TestCase
+	RstStreamFrameCase := []byte(`{
+    "error": null,
+    "wire": "00000403000000000500000008",
+    "frame": {
+        "length": 4,
+        "frame_payload": {
+            "error_code": 8,
+            "padding_length": null,
+            "padding": null
+        },
+        "flags": 0,
+        "stream_identifier": 5,
+        "type": 3
+    },
+    "draft": 14,
+    "description": "noraml rst stream frame"
+	}`)
+
+	err := json.Unmarshal(RstStreamFrameCase, &c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wire := c.Wire
+	length := c.Frame.Length
+
+	streamId := c.Frame.StreamId
+	types := c.Frame.Type
+
+	expected := NewRstStreamFrame(8, streamId)
+	expected.Length = length
+	expected.Type = types
+
+	actual, err := ReadFrame(hexToBuffer(wire))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// compare struct
+	assert.Equal(t, actual, expected)
+
+	// compare wire
+	buf := bytes.NewBuffer(make([]byte, 0))
+	expected.Write(buf)
+
+	hexdump := strings.ToUpper(hex.EncodeToString(buf.Bytes()))
+
+	assert.Equal(t, wire, hexdump)
+}
+
 func TestSettingsFrame(t *testing.T) {
 	settings := []Setting{
 		{SETTINGS_MAX_CONCURRENT_STREAMS, 100},
