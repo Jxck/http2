f128b5418862b15eca51afed94bae2b40741c2a3
Jxck <block.rxckin.beats@gmail.com>
2014-10-12 09:29:38 +0900

    add more options

--- main/server.go
+++ main/server.go
@@ -11,13 +11,18 @@ import (
 )
 
 var (
-	verbose  bool
 	loglevel int
+	dir      string
+	key      string
+	cert     string
 )
 
 func init() {
 	f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	f.IntVar(&loglevel, "l", 0, logger.Help())
+	f.StringVar(&dir, "d", ".", "document root")
+	f.StringVar(&key, "key", "keys/key.pem", "ssl key")
+	f.StringVar(&cert, "cert", "keys/cert.pem", "ssl cert")
 	f.Parse(os.Args[1:])
 	for 0 < f.NArg() {
 		f.Parse(f.Args()[1:])
@@ -35,12 +40,11 @@ $ go run main/server.go 3000
 			`)
 		}
 	}()
-	// params
-	addr := ":" + os.Args[1]
-	cert := "keys/cert.pem"
-	key := "keys/key.pem"
 
-	var handler http.Handler = http.FileServer(http.Dir("/tmp"))
+	// params
+	port := ":" + os.Args[1]
+
+	var handler http.Handler = http.FileServer(http.Dir(dir))
 
 	// setup TLS config
 	config := &tls.Config{
@@ -50,13 +54,13 @@ $ go run main/server.go 3000
 
 	// setup Server
 	server := &http.Server{
-		Addr:           addr,
+		Addr:           port,
 		Handler:        handler,
 		MaxHeaderBytes: http.DefaultMaxHeaderBytes,
 		TLSConfig:      config,
 		TLSNextProto:   http2.TLSNextProto,
 	}
 
-	fmt.Println("server starts at localhost", addr)
+	fmt.Println("server starts at localhost", port)
 	fmt.Println(server.ListenAndServeTLS(cert, key))
 }
