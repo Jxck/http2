c59631b269f7627103cd4f5ec0bb8e3c4e052d5b
Jxck <block.rxckin.beats@gmail.com>
2014-10-19 18:41:41 +0900

    add sample server

---
+++ sample/http.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/Jxck/http2"
+	"github.com/Jxck/logger"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var loglevel int = 0
+
+func init() {
+	logger.Level(loglevel)
+	logger.Debug("%v", loglevel)
+}
+
+type Person struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello world")
+}
+
+// テンプレートのコンパイル
+var t = template.Must(template.ParseFiles("index.html"))
+
+func PersonHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close() // 処理の最後にBodyを閉じる
+
+	if r.Method == "POST" {
+		// リクエストボディをJSONに変換
+		var person Person
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&person)
+		if err != nil { // エラー処理
+			log.Fatal(err)
+		}
+
+		// ファイル名を{id}.txtとする
+		filename := fmt.Sprintf("%d.txt", person.ID)
+		file, err := os.Create(filename) // ファイルを生成
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		// ファイルにNameを書き込む
+		_, err = file.WriteString(person.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// レスポンスとしてステータスコード201を送信
+		w.WriteHeader(http.StatusCreated)
+	} else if r.Method == "GET" {
+		// パラメータを取得
+		id, err := strconv.Atoi(r.URL.Query().Get("id"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		filename := fmt.Sprintf("%d.txt", id)
+		b, err := ioutil.ReadFile(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// personを生成
+		person := Person{
+			ID:   id,
+			Name: string(b),
+		}
+
+		// レスポンスにエンコーディングしたHTMLを書き込む
+		t.Execute(w, person)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/persons", PersonHandler)
+
+	// setup TLS config
+	cert := "../keys/cert.pem"
+	key := "../keys/key.pem"
+	config := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{http2.VERSION},
+	}
+
+	// setup Server
+	server := &http.Server{
+		Addr:           ":3000",
+		MaxHeaderBytes: http.DefaultMaxHeaderBytes,
+		TLSConfig:      config,
+		TLSNextProto:   http2.TLSNextProto,
+	}
+
+	fmt.Println(server.ListenAndServeTLS(cert, key))
+}
